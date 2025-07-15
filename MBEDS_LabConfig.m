ef00526b1600348558163a526e6e87ccefd89d00
Simon Kern <14980558+skjerns@users.noreply.github.com>
2025-07-15 09:00:00 +0200

    Update MBEDS_LabConfig.m

--- MBEDS_LabConfig.m
+++ MBEDS_LabConfig.m
@@ -10,13 +10,17 @@ function C = MBEDS_LabConfig
     % 2) language of SART
     C.language = "en";            % options: 'de', 'en', 'fr', 'cn', 'jp'
     
-    % 3) parallel port hex address
-    C.lpt_hex = '3FF8'; 
+    % 3) trigger setup
+    C.trigger_interface = "parallel"; % parallel or serial port?
+    C.trigger_port = '3FF8'; % e.g. LPT hex id or COM port
+    C.trigger_duration = 0.01;  % Trigger pulse duration, usually 5ms
+    C.baudrate = 2000000; % baudrate in case of serial COM port (2000000 is default for brainproducts Triggerbox Plus)
 
     % 4) debug mode
     C.debug_mode = true;       % set to false to send triggers
 
     % make sure no field is missing
-    required = ["location","lab_id", "language", "lpt_hex","debug_mode"];
+    required = ["location","lab_id", "language", "lpt_hex","debug_mode", 
+                "trigger_interface", "trigger_duration", "trigger_port"];
     assert(all(isfield(C, required)), 'Config file missing fields');
 end
